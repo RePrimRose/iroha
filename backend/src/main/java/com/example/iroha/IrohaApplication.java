5df9abf3ca3c3e37f14300eec38b160fbfdc9459
RePrimrose <park1088230@gmail.com>
2024-12-05 02:48:47 +0900

    first commit

---
+++ backend/src/main/java/com/example/iroha/IrohaApplication.java
@@ -0,0 +1,13 @@
+package com.example.iroha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrohaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrohaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/iroha/IrohaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iroha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrohaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
