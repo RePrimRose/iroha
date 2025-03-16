7e9fa8ef1efccf1d1aa3b80a14466f0f4c445511
RePrimRose <park1088230@gmail.com>
2025-03-16 16:38:26 +0900

    docs: update README

--- frontend/src/pages/Main/NormalCardSection.js
+++ frontend/src/pages/Main/NormalCardSection.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const NormalCardSection = () => {
     return (
@@ -14,9 +15,9 @@ const NormalCardSection = () => {
             {/* 카드 2 */}
             <div className="bg-gray-100 p-6 rounded-lg shadow-lg text-center">
                 <div className="text-5xl text-traditionalBlue mb-4">🖋️</div>
-                <h3 className="text-xl font-bold mb-2">문장 연습</h3>
+                <h3 className="text-xl font-bold mb-2">테스트</h3>
                 <p className="text-gray-700">
-                    다양한 문장으로 문법과 작문 실력을 쌓아보세요.
+                    여러 유형의 테스트 중 원하는 테스트를 선택해서 일본어 실력을 키워보세요.
                 </p>
             </div>
         </div>
